8a447b68a310316fdb7e781c9bf5104fcbc17530
"wbogatyrewa" <"veronika01-01@mail.ru">
2022-05-28 13:27:37 +0500

    requirements added

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -13,8 +13,10 @@ contract Voting {
     uint endTime;
 
     mapping (address => bool) voters; // {address of voter => voted}
+    mapping (address => bool) votersExist;
 
     mapping (address => string) proposals; // {address of proposal => name of proposal}
+    mapping (address => bool) proposalsExist;
 
     address chairperson;
 
@@ -41,12 +43,19 @@ contract Voting {
 
     function addVoters(address[] memory _voters) public onlyChairperson {
         for (uint i = 0; i < _voters.length; i++) {
+            require(!proposalsExist[_voters[i]], "Proposal can't be voter.");
+            require(!votersExist[_voters[i]], "Voter exists.");
+    
             voters[_voters[i]] = false;
+            votersExist[_voters[i]] = true;
             token.mint(_voters[i], 1); 
         }
-        if (token.balanceOf(chairperson) == 0) {
-            token.mint(chairperson, 1); 
-        }
+        require(!proposalsExist[chairperson], "Proposal can't be chairperson.");
+        require(!votersExist[chairperson], "Voter exists.");
+        voters[chairperson] = false;
+        votersExist[chairperson] = true;
+        token.mint(chairperson, 1); 
+        
     }
 
     function addProposals(address[] memory _proposalsAddr, 
@@ -54,18 +63,20 @@ contract Voting {
         require(_proposalsAddr.length == _proposalsNames.length, "Length is different.");
 
         for (uint i = 0; i < _proposalsAddr.length; i++) {
-            if (msg.sender != _proposalsAddr[i]) {
-                proposals[_proposalsAddr[i]] = _proposalsNames[i];
-            }
+            require(msg.sender != _proposalsAddr[i], "Chairperson can't be proposal.");
+            require(!votersExist[_proposalsAddr[i]], "Voter can't be proposal.");
             
+            proposals[_proposalsAddr[i]] = _proposalsNames[i];
+            proposalsExist[_proposalsAddr[i]] = true;
         }
     }
 
     function vote(address proposal) public voteIsOn {
+        require(votersExist[msg.sender], "Voter doesn't exist.");
         require(token.balanceOf(msg.sender) > 0, "Token doesn't exist.");
+        require(proposalsExist[proposal], "Proposal doesn't exist.");
         require(!voters[msg.sender], "Voter already voted.");
-        require(bytes(proposals[proposal]).length > 0 , "This address does not exist among the candidates.");
-        //require(voter[msg.sender], "Voter doesn't exist.");
+        require(!votersExist[proposal], "Voter can't be proposal.");
         
         token.transfer(proposal, token.balanceOf(msg.sender));
         voters[msg.sender] = true;
@@ -73,7 +84,14 @@ contract Voting {
 
     function totalVotesFor(address proposal) view public returns (uint256) {
         require(block.timestamp >= endTime, "Voting has not ended.");
-        require(bytes(proposals[proposal]).length > 0 , "This address does not exist among the candidates.");
+        require(proposalsExist[proposal], "Proposal doesn't exist.");
+        
         return token.balanceOf(proposal);
     }
-}
+
+    function nameOfProposal(address proposal) view public returns (string memory) {
+        require(proposalsExist[proposal], "Proposal doesn't exist.");
+        
+        return proposals[proposal];
+    }
+} 
